1727ce880d2927ca3e0f393c3c651c16eed987a1
Norsang Nyandak <TNorsang>
2022-12-11 18:03:39 -0500

    added new

--- getChat.php
+++ getChat.php
@@ -11,8 +11,8 @@ $message = $_POST['messages'];
 $selectChat = "SELECT * FROM `Chat` WHERE 1";
 $result = mysqli_query($conn, $selectChat);
 
-if(mysqli_num_rows($result)>0){
-    while($row = mysqli_fetch_array($result)){
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_array($result)) {
         echo '<h4>' . $row["Name"] . '</h1>' . '<p>' . $row["ChatContent"] . '</p>';
     }
 }
--- insert.php
+++ insert.php
@@ -10,12 +10,12 @@ $query = "SELECT * FROM `Chat` WHERE Name='$user'";
 $re = mysqli_query($conn, $query);
 $row1 = $re->fetch_assoc();
 
-if($row1['Name'] == $user && $row1['Password'] == $password){
+if ($row1['Name'] == $user && $row1['Password'] == $password) {
     $insertChat = "UPDATE `Chat` SET `ChatContent`= '$message' WHERE Name = '$user'";
     $result = mysqli_query($conn, $insertChat);
     echo "Welcome $user! Your message has been send!";
     // $row = $result->fetch_assoc();
-}else{
+} else {
     // echo "This is Data'$row1[Name]'";
     // echo "This is Data '$row1[Password]'";
     // echo "User Input $user";
