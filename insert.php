1b62ba8c1ab2d451df7d97a1979fce2041f2d868
git config --global user.name TNorsang <thenorsang@gmail.com>
2022-12-10 20:45:15 -0500

    finished

--- insert.php
+++ insert.php
@@ -4,18 +4,23 @@ include("dbConnect.php");
 
 $user = $_POST['users'];
 $message = $_POST['messages'];
+$password = $_POST['passwords'];
 
-// echo $user;
-// echo $message;
+$query = "SELECT * FROM `Chat` WHERE Name='$user'";
+$re = mysqli_query($conn, $query);
+$row1 = $re->fetch_assoc();
 
-$insertChat = "UPDATE `Chat` SET `ChatContent`= '$message' WHERE Name = '$user'";
-$result = mysqli_query($conn, $insertChat);
+if($row1['Name'] == $user && $row1['Password'] == $password){
+    $insertChat = "UPDATE `Chat` SET `ChatContent`= '$message' WHERE Name = '$user'";
+    $result = mysqli_query($conn, $insertChat);
+    echo "Welcome $user! Your message has been send!";
+    // $row = $result->fetch_assoc();
+}else{
+    // echo "This is Data'$row1[Name]'";
+    // echo "This is Data '$row1[Password]'";
+    // echo "User Input $user";
+    echo "Your Name or Password is Incorrect!";
+}
 
-
-// echo "<script>alert('$user');</script>";
-
-// $name=$_POST["names"];
-
-// echo $_POST["messages"];
 
 ?>
